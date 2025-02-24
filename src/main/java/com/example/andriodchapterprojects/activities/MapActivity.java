65d599e315cdb1ce9e68101e7a0bddc5dbec00cf
Avb2 <bringuel.alexander@gmail.com>
2025-02-23 20:52:58 -0500

    .

--- src/main/java/com/example/andriodchapterprojects/activities/MapActivity.java
+++ src/main/java/com/example/andriodchapterprojects/activities/MapActivity.java
@@ -1,16 +1,17 @@
 package com.example.andriodchapterprojects.activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,21 +19,20 @@ import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.andriodchapterprojects.R;
 
-import java.io.IOException;
-import java.util.List;
-
 public class MapActivity extends AppCompatActivity {
 
     LocationManager locationManager;
 
     LocationListener gpsListener;
-
+    final int PERMISSION_REQUEST_LOCATION = 101;
 
 
     @Override
@@ -43,13 +43,70 @@ public class MapActivity extends AppCompatActivity {
 //        setToolbarTitle();
         initListButton();
         initSettingsButton();
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_contact_map), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
 
+    private void startLocationUpdates() {
+        try {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (ContextCompat.checkSelfPermission(MapActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            MapActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    )) {
+                        Snackbar.make(findViewById(R.id.activity_contact_map),
+                                "MyContactList requires this permission to locate " +
+                                        "your contacts", Snackbar.LENGTH_INDEFINITE)
+                                .setAction("Ok", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        ActivityCompat.requestPermissions(
+                                                MapActivity.this,
+                                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        PERMISSION_REQUEST_LOCATION
+                                                );
+
+                                    }
+                                })
+                                .show();
+
+                    } else {
+                        ActivityCompat.requestPermissions(
+                                MapActivity.this,
+                                new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                                PERMISSION_REQUEST_LOCATION);
+                    }
+                } else {
+                    startLocationUpdates();
+                }
+            } else {
+                startLocationUpdates();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getBaseContext(), "Error requesting permission",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, String permissions[], int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdates();
+                } else  {
+                    Toast.makeText(MapActivity.this, "MyContactList will not locate your contacts",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
 
     @Override
     public void onPause() {
