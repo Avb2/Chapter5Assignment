88ef437675314093a6b022ded18dafe6b7024786
Avb2 <bringuel.alexander@gmail.com>
2025-02-23 20:21:35 -0500

    Update

--- src/main/java/com/example/andriodchapterprojects/activities/MapActivity.java
+++ src/main/java/com/example/andriodchapterprojects/activities/MapActivity.java
@@ -1,9 +1,19 @@
 package com.example.andriodchapterprojects.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +24,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.andriodchapterprojects.R;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+
+    LocationListener gpsListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,47 @@ public class MapActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            locationManager.removeUpdates(gpsListener);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    ///
+    private void initGetLocationButton() {
+        Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    locationManager = (LocationManager) getBaseContext().getSystemService(Context.LOCATION_SERVICE);
+                    gpsListener = new LocationListener() {
+                        public void onLocationChanged(Location location) {
+                            TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
+                            TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
+                            TextView txtAccuracy = (TextView) findViewById(R.id.textAccuracy);
+
+                            txtLatitude.setText(String.valueOf(location.getLatitude()));
+                            txtLongitude.setText(String.valueOf(location.getLongitude()));
+                            txtAccuracy.setText(String.valueOf(location.getAccuracy()));
+                        }
+                        public void onStatusChanged(String provider, int status, Bundle extras){}
+                        public void onProviderEnabled(String provider) {}
+                        public void onProviderDisabled(String provider){}
+                    };
+                } catch (Exception e) {
+                    Toast.makeText(getBaseContext(), "Error, Location not available", Toast.LENGTH_LONG).show();
+
+                }
+            }
         });
     }
 
